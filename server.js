2e6b92ebdd237ee4911e8857462a4cb0812b7b1c
sandeep8093 <nayaksandeep676@gmail.com>
2023-11-01 22:03:34 +0530

    main

--- server.js
+++ server.js
@@ -16,7 +16,9 @@ mongoose
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
-
+app.get('/', function (req, res) {
+  res.send('hello world')
+  })
 app.use('/user', require('./src/routers/user.routes'));
 app.use('/language', require('./src/routers/language.routes'));
 app.use('/exercise', require('./src/routers/exercise.routes'));
